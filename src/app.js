14e3510d4138d2ecf9bf98aa728dd0ab71546378
Eduardo Dedios <eduardodediosp96@gmail.com>
2023-08-14 01:28:39 -0500

    removing swagger

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import * as dotenv from "dotenv";
 import express from "express";
 import bodyparser from "body-parser";
 //swagger
-import swaggerUi from "swagger-ui-express";
-import swaggerJsDoc from "swagger-jsdoc";
-import { swaggerSpec } from "./config/swagger/swaggerSpec";
+// import swaggerUi from "swagger-ui-express";
+// import swaggerJsDoc from "swagger-jsdoc";
+// import { swaggerSpec } from "./config/swagger/swaggerSpec";
 import routes from "./routes";
 
 const app = express();
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use("/", routes);
-app.use(
-  "/api/docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerJsDoc(swaggerSpec))
-);
+// app.use(
+//   "/api/docs",
+//   swaggerUi.serve,
+//   swaggerUi.setup(swaggerJsDoc(swaggerSpec))
+// );
 // app.use(clientErrorHandler);
 // app.use(errorHandler);
 
